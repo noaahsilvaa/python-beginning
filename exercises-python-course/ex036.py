ab2769304bb2cef211019d70090d179ab8e4108b
Giovana Silva <85005281+giovansilva@users.noreply.github.com>
2022-04-27 19:22:01 -0300

    ex 36 add

--- exercises-python-course/ex036.py
+++ exercises-python-course/ex036.py
@@ -1,3 +1,11 @@
-valcasa = float(input('Qual o valor da casa? R$'))
-salcomp = float(input('Qual o salário do comprador? R$'))
-qntano = float(input('Em quantos anos vai ser pago? '))
+val= float(input('Qual o valor da casa? R$'))
+sal= float(input('Qual o salário do comprador? R$'))
+anos= float(input('Em quantos anos vai ser pago? '))
+prest= val/(anos*12)
+sal30= (30/100*sal)
+if prest > sal30:
+    print('A PRESTAÇÃO CALCULADA EXCEDE 30% DO SEU SALÁRIO!')
+    print('EMPRÉSTIMO NEGADO!')
+else:
+    print('DE ACORDO COM SEU SALÁRIO, VOCÊ PODE PAGAR')
+    print('EMPRÉSTIMO APROVADO!')
